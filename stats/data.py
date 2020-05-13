c936aef99d529db97096ed95592e5649de8bb192
Jason Fincham <jason.fincham@draftserv.com>
2020-05-13 09:48:11 -0400

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -2,7 +2,6 @@ import os
 import glob
 import pandas as pd
 
-# game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE')).sort()
 game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
 
